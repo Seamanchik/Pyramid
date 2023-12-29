5d0a54f8f645d8a6218cb2dbe6622aa02df169ab
vladr <vladryabyshev@gmail.com>
2023-12-30 01:23:34 +0300

    First commit

---
+++ Pyramid/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Pyramid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 471);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseUp);
+            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.PictureBox_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(544, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnCreatePyramid_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(677, 467);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        #endregion
+    }
+}
---
+++ Pyramid/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Pyramid
+{
+    public partial class Form1 : Form
+    {
+        private Pyramid _pyramid;
+        private Point _lastMousePos;
+        private bool _isLeftMouseDown;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (_pyramid != null)
+                _pyramid.Draw(e.Graphics);
+        }
+
+        private void PictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _isLeftMouseDown = true;
+                _lastMousePos = e.Location;
+            }
+        }
+
+        private void PictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isLeftMouseDown && _pyramid != null)
+            {
+                int deltaX = e.X - _lastMousePos.X;
+                int deltaY = e.Y - _lastMousePos.Y;
+
+                _pyramid.RotateY(deltaX * 0.01f);
+                _pyramid.RotateX(deltaY * 0.01f);
+
+                _lastMousePos = e.Location;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void PictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _isLeftMouseDown = false;
+            }
+        }
+
+        private void btnCreatePyramid_Click(object sender, EventArgs e)
+        {
+            _pyramid = new Pyramid(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Invalidate();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            HandleKey(e.KeyCode, true);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            HandleKey(e.KeyCode, false);
+        }
+        
+        private void PictureBox_Scroll(object sender, MouseEventArgs e)
+        {
+            if (_pyramid != null)
+            {
+                float deltaZoom = e.Delta > 0 ? 1.1f : 0.9f;
+                _pyramid.Zoom(deltaZoom);
+                pictureBox1.Invalidate();
+            }
+        }
+        
+        private void HandleKey(Keys key, bool isKeyDown)
+        {
+            if (_pyramid != null)
+            {
+                float delta = 0.1f;
+
+                switch (key)
+                {
+                    case Keys.W:
+                        _pyramid.RotateX(isKeyDown ? delta : 0);
+                        break;
+                    case Keys.S:
+                        _pyramid.RotateX(isKeyDown ? -delta : 0);
+                        break;
+                    case Keys.A:
+                        _pyramid.RotateY(isKeyDown ? -delta : 0);
+                        break;
+                    case Keys.D:
+                        _pyramid.RotateY(isKeyDown ? delta : 0);
+                        break;
+                }
+
+                pictureBox1.Invalidate();
+            }
+        }
+    }
+
+    class Point3D
+    {
+        private float X { get; set; }
+        private float Y { get; set; }
+        private float Z { get; set; }
+
+        public Point3D(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public void RotateX(float angle) //Вращение вокруг оси Х
+        {
+            float newY = (float)(Y * Math.Cos(angle) - Z * Math.Sin(angle));
+            float newZ = (float)(Y * Math.Sin(angle) + Z * Math.Cos(angle));
+            Y = newY;
+            Z = newZ;
+        }
+
+        public void RotateY(float angle) //Вращение вокруг оси Y
+        {
+            float newX = (float)(X * Math.Cos(angle) + Z * Math.Sin(angle));
+            float newZ = (float)(-X * Math.Sin(angle) + Z * Math.Cos(angle));
+            X = newX;
+            Z = newZ;
+        }
+
+        public void Zoom(float factor)
+        {
+            X *= factor;
+            Y *= factor;
+            Z *= factor;
+        }
+
+        public Point To2D()
+        {
+            return new Point((int)X + 200, (int)Y + 190);
+        }
+    }
+
+    class Pyramid
+    {
+        private readonly Point3D[] _vertices;
+
+        public Pyramid(float width, float height)
+        {
+            width /= 6;
+            height /= 6;
+            
+            _vertices = new[]
+            {
+                new Point3D(width, height, width), //Левая нижняя вершина
+                new Point3D(-width, height, width), //Правая верхняя вершина
+                new Point3D(-width, height, -width), //Правая нижняя вершина
+                new Point3D(width, height, -width), //Левая верхняя вершина
+                new Point3D(0, -height, 0), //Верхняя вершина
+            };
+        }
+
+        public void RotateX(float angle)
+        {
+            foreach (var vertex in _vertices)
+            {
+                vertex.RotateX(angle);
+            }
+        }
+
+        public void RotateY(float angle)
+        {
+            foreach (var vertex in _vertices)
+            {
+                vertex.RotateY(angle);
+            }
+        }
+
+        public void Zoom(float factor)
+        {
+            foreach (var vertex in _vertices)
+            {
+                vertex.Zoom(factor);
+            }
+        }
+
+        public void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+
+            for (int i = 0; i < 4; i++)
+            {
+                g.DrawLine(pen, _vertices[i].To2D(), _vertices[(i + 1) % 4].To2D());
+                g.DrawLine(pen, _vertices[i].To2D(), _vertices[4].To2D());
+            }
+        }
+    }
+}
